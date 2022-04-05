ee6b03006dc306e1c8f810a1daa1ad9961cf1cd6
FabianAMoreno <101095654+FabianAMoreno@users.noreply.github.com>
2022-04-05 12:42:32 +0200

    refined solution

--- app/src/main/java/de/syntax_institut/filmestreamingservice/data/Datasource.kt
+++ app/src/main/java/de/syntax_institut/filmestreamingservice/data/Datasource.kt
@@ -10,37 +10,49 @@ import kotlin.random.Random
 class Datasource(private val context: Context) {
 
     private val nrOfImages = 14 // anpassen, wenn Anzahl an Bildern geändert wurde
-    private val nrOfTitles = 30 // anpassen, wenn Anzahl an Filmtiteln geändert wurde
+    private val numberOfTitles = 30 // anpassen, wenn Anzahl an Filmtiteln geändert wurde
 
     /**
      * Diese Funktion holt die Titel & Bilder aus der Quelldatei
      * und liefert eine Liste aus Filmen zurück
      */
     fun loadMovies(): List<Movie> {
+        // Die Liste an Filmen
         val movies = mutableListOf<Movie>()
-        for (i in 1..nrOfTitles) {
+
+        // Befülle die Liste
+        for (index in 1..numberOfTitles) {
+
+            // Hole den Titel und das Bild
+            val title = getTitle(index)
+            val image = getImage()
+
+            // Füge ein Movie Objekt hinzu
             movies.add(
-                // Füge Movie Objekt hinzu
-                Movie(
-                    context.resources.getIdentifier(
-                        "movieTitle$i",
-                        "string",
-                        context.packageName
-                    ),
-                    randomImage()
-                )
+
+                Movie(title, image)
+
             )
         }
         return movies
     }
 
-    // für randomImage
-    private var iPrevious = 0
+    /**
+     * Diese Funktion liefert den Titel Nummer index aus der Quelle
+     */
+    private fun getTitle(index: Int): Int {
+        return context.resources.getIdentifier(
+            "movieTitle$index",
+            "string",
+            context.packageName
+        )
+    }
 
+    private var iPrevious = 0
     /**
      * Diese Funktion liefert ein zufälliges Bild aus der Bilderquelle
      */
-    private fun randomImage(): Int {
+    private fun getImage(): Int {
         var i: Int
         // kein Bild soll zweimal hintereinander kommen
         while (true) {
@@ -50,7 +62,7 @@ class Datasource(private val context: Context) {
                 break
             }
         }
-        // Liefere das entsprechende Bil aus der Quelle
+        // Liefere das entsprechende Bild aus der Quelle
         return context.resources.getIdentifier(
             "film_$i",
             "drawable",
