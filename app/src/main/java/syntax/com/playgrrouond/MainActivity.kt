9a9eb47ad97a0b91a5b574e81cf4d5d55f7cc8e2
michaelSyntax <michael.karbe@syntax-institut.de>
2024-02-05 00:16:05 +0100

    Add clickListener for all buttons. Add functions to reset all views.

--- app/src/main/java/syntax/com/playgrrouond/MainActivity.kt
+++ app/src/main/java/syntax/com/playgrrouond/MainActivity.kt
@@ -1,5 +1,6 @@
 package syntax.com.playgrrouond
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import syntax.com.playgrrouond.databinding.ActivityMainBinding
@@ -11,5 +12,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        resetAllViews()
+        setupAllButtons()
+    }
+
+    private fun setupAllButtons() {
+        binding.buttonGenerateText.setOnClickListener {
+            val editTextEntry = binding.editTextText.text
+            binding.textViewResult.text = getString(R.string.textViewOutput) + "\n${editTextEntry}"
+        }
+        binding.buttonReset.setOnClickListener {
+            resetAllViews()
+        }
+    }
+
+    private fun resetAllViews() {
+        setAllBackgroundColorToWhite()
+        setAllSwitchesTo(true)
+        resetTextViews()
+        setAllRadioButtonsTo(false)
+    }
+
+    private fun setAllBackgroundColorToWhite() {
+        binding.root.setBackgroundColor(Color.WHITE)
+        binding.linearLayoutEditText.setBackgroundColor(Color.WHITE)
+        binding.linearLayoutRadioButtons.setBackgroundColor(Color.WHITE)
+        binding.linearLayoutSwitches.setBackgroundColor(Color.WHITE)
+        binding.linearLayoutButtons.setBackgroundColor(Color.WHITE)
+        binding.linearLayoutTextViewResult.setBackgroundColor(Color.WHITE)
+    }
+
+    private fun setAllSwitchesTo(isChecked: Boolean) {
+        binding.switchEditText.isChecked = isChecked
+        binding.switchRadioButtons.isChecked = isChecked
+        binding.switchButton.isChecked = isChecked
+    }
+
+    private fun setAllRadioButtonsTo(isChecked: Boolean) {
+        binding.radioButtonRed.isChecked = isChecked
+        binding.radioButtonGreen.isChecked = isChecked
+        binding.radioButtonBlue.isChecked = isChecked
+    }
+
+    private fun resetTextViews() {
+        binding.editTextText.setText("")
+        binding.textViewResult.text = getString(R.string.textViewOutput)
     }
 }
