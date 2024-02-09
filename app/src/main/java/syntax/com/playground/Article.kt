d49897699dbadb766055f7c373593abb4bd7ff76
michaelSyntax <michael.karbe@syntax-institut.de>
2024-02-09 13:37:12 +0100

    Reset project.

--- app/src/main/java/syntax/com/playground/Article.kt
+++
@@ -1,3 +0,0 @@
-package syntax.com.playground
-
-data class Article(val image: Int, val text: String, val isShip: Boolean)
--- app/src/main/java/syntax/com/playground/MainActivity.kt
+++ app/src/main/java/syntax/com/playground/MainActivity.kt
@@ -12,8 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val articleList = DataSource().loadArticels()
-        binding.recyclerView.adapter = ArticleAdapter(articleList)
     }
 }
